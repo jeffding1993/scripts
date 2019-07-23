0449688727078536ecf68dee8cc24fd9ff002c05
jeff.ding <jeffding1993@outlook.com>
2019-07-23 14:36:37 +0800

    change multithreading

--- auto_sw_backup/main.py
+++ auto_sw_backup/main.py
@@ -1,12 +1,30 @@
 from auto_backup_sw import *
 import auto_backup_asa
 from settings import *
+from concurrent.futures import ThreadPoolExecutor
+import time
+
+
+def switch_backup(host):
+    backup_sw(host)
+    modify_backup(host)
+
+
+def asa_backup(host):
+    auto_backup_asa.backup_asa(host)
+    auto_backup_asa.modify_backup(host)
+
 
 if __name__ == '__main__':
+    # 使用多进程提高运行效率
+    p = ThreadPoolExecutor()
+
+    start_time = time.time()
+
     for host in HUAWEI_HOSTS:
-        backup_sw(host)
-        modify_backup(host)
+        p.submit(switch_backup, host)
 
     for host in CISCO_HOSTS:
-        auto_backup_asa.backup_asa(host)
-        auto_backup_asa.modify_backup(host)
+        p.submit(asa_backup, host)
+    p.shutdown()
+    print("use time: %s " % (time.time() - start_time,))
